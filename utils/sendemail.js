7bf707297f31aa33cc1173d769dad13d38149c29
Taylor Farley <twfarley88@gmail.com>
2020-11-23 19:59:31 -0500

    fixin login

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -2,14 +2,7 @@ const sgMail = require('@sendgrid/mail');
 
 const api=process.env.API
 sgMail.setApiKey(api);
-// const msg = {
-//     to: 'twfarley88@gmail.com',
-//     from: 'twfarley88@gmail.com',
-//     subject: 'Your Meal Prep',
-//     text: 'message',
-//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-//   };
-  // sgMail.send(msg); 
+
 
 
 module.exports = {
